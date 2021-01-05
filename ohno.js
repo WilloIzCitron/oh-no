82f6497c491632ecc892370bca83a93d5d9a25b5
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-01-05 10:47:07 +0700

    fixes

--- ohno.js
+++ ohno.js
@@ -5,25 +5,25 @@ const client = new Discord.Client();
 let ohnoCount = 0;
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    const ohno = message.guild.emojis.cache.find(x => x.name === "ohno")
-	if(!ohno){
-		 message.guild.emojis.create("https://cdn.discordapp.com/emojis/597477759689687040.png?v=1", "ohno") 
-		 message.channel.send("ohno emote created")
-	         }
-	if(msg.content.toLowerCase().includes("oh no")){
-		msg.react(ohno);
-		ohnoCount++;
-		client.user.setPresence({
-			activity: { 
-				name: `${ohnoCount} oh no`
-			},
-			status: 'online'
-		})
-	}
+    const ohno = msg.guild.emojis.cache.find(x => x.name === "ohno")
+    if (!ohno) {
+        msg.guild.emojis.create("https://cdn.discordapp.com/emojis/597477759689687040.png?v=1", "ohno")
+        msg.channel.send("ohno emote created")
+    }
+    if (msg.content.toLowerCase().includes("oh no")) {
+        msg.react(ohno);
+        ohnoCount++;
+        client.user.setPresence({
+            activity: {
+                name: `${ohnoCount} oh no`
+            },
+            status: 'online'
+        })
+    }
 });
 
 client.login(process.env.ohno_BOT_TOKEN);
