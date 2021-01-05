8fcdc0917fa0fdae25b572e6df8ffa3011cd4cfd
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-01-05 10:17:06 +0700

    djs 12

--- ohno.js
+++ ohno.js
@@ -2,7 +2,7 @@ require("dotenv"); // Load env configuration
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const ohno = message.guild.emojis.cache.find(x => x.name === "ohno")
 let ohnoCount = 0;
 
 client.on('ready', () => {
@@ -11,11 +11,11 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if(msg.content.toLowerCase().includes("oh no")){
-		msg.react('<:ohno:597477759689687040>');
+		msg.react(ohno);
 		ohnoCount++;
 		client.user.setPresence({
 			activity: { 
-				name: `${ohnoCount} <:ohno:597477759689687040>`
+				name: `${ohnoCount} oh no`
 			},
 			status: 'online'
 		})
