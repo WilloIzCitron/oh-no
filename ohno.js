d40c4ef5c48507eb446d3c93b310bcbaa81ef27f
Tegas Aziz <49786146+WilloIzCitron@users.noreply.github.com>
2021-02-17 15:47:10 +0700

    added web host

--- ohno.js
+++ ohno.js
@@ -1,3 +1,15 @@
+//|||||||||||||||||||||||WEB|||||||||||||||||||||||
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+//|||||||||||||||||||||||BOT|||||||||||||||||||||||
+
 require("dotenv"); // Load env configuration
 
 const Discord = require('discord.js');
@@ -9,21 +21,26 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if(msg.author.bot)return;
+    if (msg.author.bot) return;
     const ohno = msg.guild.emojis.cache.find(x => x.name === "ohno")
-    if (!ohno) {
-        msg.guild.emojis.create("https://cdn.discordapp.com/emojis/597477759689687040.png?v=1", "ohno")
-        msg.channel.send("ohno emote created")
-    }
     if (msg.content.toLowerCase().includes("oh no")) {
-        msg.react(ohno);
-        ohnoCount++;
-        client.user.setPresence({
-            activity: {
-                name: `${ohnoCount} oh no`
-            },
-            status: 'online'
-        })
+        if (!ohno) {
+            if (msg.guild.emojis.cache.size > 100) {
+                return
+            }
+            msg.guild.emojis.create("https://cdn.discordapp.com/emojis/597477759689687040.png?v=1", "ohno")
+            msg.channel.send("ohno emote created")
+        } else {
+            msg.react(ohno);
+            ohnoCount++;
+            client.user.setPresence({
+                activity: {
+                    name: `Over ${ohnoCount} oh nos`,
+                    type: 'WATCHING'
+                },
+                status: 'online'
+            })
+        }
     }
 });
 
